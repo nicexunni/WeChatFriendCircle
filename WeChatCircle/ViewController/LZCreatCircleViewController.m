dfe1a0c36d9928a99f5b032bf59e3bcf653cb7ce
nicexunni <1550754077@qq.com>
2018-03-15 13:49:33 +0800

    add creat VC

--- WeChatCircle/ViewController/LZCreatCircleViewController.m
+++ WeChatCircle/ViewController/LZCreatCircleViewController.m
@@ -1,33 +1,23 @@
 //
-//  LZHomeViewController.m
+//  LZCreatCircleViewController.m
 //  WeChatCircle
 //
 //  Created by xunni on 2018/3/15.
 //  Copyright © 2018年 xunni. All rights reserved.
 //
 
-#import "LZHomeViewController.h"
 #import "LZCreatCircleViewController.h"
 
-@interface LZHomeViewController ()
+@interface LZCreatCircleViewController ()
 
 @end
 
-@implementation LZHomeViewController
+@implementation LZCreatCircleViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.navigationItem.title = @"首页";
-    UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:@"创建" style:UIBarButtonItemStylePlain target:self action:@selector(creatCircle)];
-    [self.navigationItem setRightBarButtonItem:rightItem];
+    self.navigationItem.title = @"创建";
     // Do any additional setup after loading the view from its nib.
-}
-
-
-- (void)creatCircle
-{
-    LZCreatCircleViewController *creatVC = [[LZCreatCircleViewController alloc] init];
-    [self.navigationController pushViewController:creatVC animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
