c123ec65ea52c24e828ecf280842764a5f93ae0c
nicexunni <1550754077@qq.com>
2018-03-15 10:27:52 +0800

    pod  add  framework

--- Podfile
+++ Podfile
@@ -3,5 +3,13 @@ platform :ios,'8.0'
 target 'WeChatCircle' do
 
 pod 'AFNetworking', '~> 3.0'
+pod 'Masonry'
+pod 'JRDB'
+pod 'YYKit'
+pod 'MMPopupView'
+pod 'CYLTabBarController', '~> 1.17.4'
+pod 'LCProgressHUD'
+pod 'JFImagePicker'
+pod 'IDMPhotoBrowser'
 
 end
--- Pods/Target Support Files/Pods-WeChatCircle/Pods-WeChatCircle-resources.sh
+++ Pods/Target Support Files/Pods-WeChatCircle/Pods-WeChatCircle-resources.sh
@@ -83,6 +83,16 @@ EOM
       ;;
   esac
 }
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "${PODS_ROOT}/IDMPhotoBrowser/Classes/IDMPhotoBrowser.bundle"
+  install_resource "${PODS_ROOT}/IDMPhotoBrowser/Classes/IDMPBLocalizations.bundle"
+  install_resource "${PODS_ROOT}/LCProgressHUD/LCProgressHUD/LCProgressHUD.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "${PODS_ROOT}/IDMPhotoBrowser/Classes/IDMPhotoBrowser.bundle"
+  install_resource "${PODS_ROOT}/IDMPhotoBrowser/Classes/IDMPBLocalizations.bundle"
+  install_resource "${PODS_ROOT}/LCProgressHUD/LCProgressHUD/LCProgressHUD.bundle"
+fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
